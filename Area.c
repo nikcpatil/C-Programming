b88de9106804359c9c5535a32a537144b6c65e89
nikcpatil <92070128+nikcpatil@users.noreply.github.com>
2023-09-07 20:19:19 +0530

    Area Calculation Program

---
+++ Area.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    float L, B, Area;
+    printf("Enter the length: ");
+    scanf("%f", &L);
+    printf("Enter the Breadth: ");
+    scanf("%f", &B);
+
+    Area= L * B;
+    printf("Area of Rectangle is: %f", Area);
+}
