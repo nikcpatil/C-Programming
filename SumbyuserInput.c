bef4941eff315b9adac3b268213e14d3a12561fd
nikcpatil <92070128+nikcpatil@users.noreply.github.com>
2023-09-07 20:27:05 +0530

    Sum of two numbers

---
+++ SumbyuserInput.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    int a, b, res;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    res= a + b;
+    printf("Result is: %d", res);
+}
