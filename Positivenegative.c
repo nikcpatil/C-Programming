50d4bc1943e94924826d987099ae87ccd49ea449
nikcpatil <92070128+nikcpatil@users.noreply.github.com>
2023-09-08 21:45:37 +0530

    Check number is +ve, -ve or zero

---
+++ Positivenegative.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+    int num;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    if(num>0){
+        printf("The number %d is positive", num);
+    }
+    else if(num<0){
+        printf("The number %d is negative", num);
+    }
+    else{
+        printf("The number %d is zero", num);
+    }
+}
