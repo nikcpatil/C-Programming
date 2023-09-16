e13245b91829ed054e219c1865984ac11409362d
nikcpatil <92070128+nikcpatil@users.noreply.github.com>
2023-09-16 22:23:31 +0530

    2D Array

---
+++ Matrix.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main(){
+    int arr[2][3] = {12, 1 , 23, 54, 32, 11};
+
+    printf("%d\n", arr[0][0]);
+    printf("%d\n", arr[0][1]);
+    printf("%d\n", arr[0][2]);
+    printf("%d\n", arr[1][0]);
+    printf("%d\n", arr[1][1]);
+    printf("%d\n", arr[1][2]);
+
+printf("*****Second matrix*****\n");
+
+    int a[2][3]= {{1, 4, 7}, {2, 6, 9}};
+
+    printf("%d\n", a[0][0]);
+    printf("%d\n", a[0][1]);
+    printf("%d\n", a[0][2]);
+    printf("%d\n", a[1][0]);
+    printf("%d\n", a[1][1]);
+    printf("%d\n", a[1][2]);
+
+}
