7293a54cf7d15e7c2b85c020ef87d24d2c970eb1
nikcpatil <92070128+nikcpatil@users.noreply.github.com>
2023-09-27 11:45:29 +0530

    Conditional operator

--- conditionby3.c
+++ conditionby3.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
-void main(){
-    int a=12, b=13, c=14;
-    printf("Maximim num is %d\n", (a>b>c)? a:b);
+#include <stdio.h>  
+   
+int main()  {  
+    int a, b, c, max;  
+    printf("Enter Three Integers\n");  
+    scanf("%d %d %d", &a, &b, &c);  
+     
+    max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+    printf("Maximum Number is = %d\n", max);  
+   
+    return 0;  
 }
 
